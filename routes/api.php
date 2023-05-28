2bd7ce8655259a8380a0f6fb7ff8aed11e666cf5
Frans <frans_421_12@hotmail.com>
2023-05-27 22:46:00 -0500

    feat: Definiendo rutas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,27 @@
 <?php
 
-echo "soy la ruta";
+$arrayRoutes = explode("/",$_SERVER['REQUEST_URI']);
+
+echo "<pre>"; print_r($arrayRoutes); echo "<pre>";
+
+if (count(array_filter($arrayRoutes)) == 2) {
+    $json = array("detalle" => "no encontrado");
+    echo json_encode($json, true);
+    return;
+} else {
+    if (count(array_filter($arrayRoutes)) == 3) {
+        if (array_filter($arrayRoutes)[3] == "cursos") {
+            $json = array("detalle" => "estas en la vista cursos");
+            echo json_encode($json, true);
+            return;
+        }
+
+        if (array_filter($arrayRoutes)[3] == "registro") {
+            $json = array("detalle" => "estas en la vista registro");
+            echo json_encode($json, true);
+            return;
+        }
+    }
+}
 
 ?>
